ff2d170c482b6ee5157c819bd887aceb3d6f8c3c
Mr Thinh <33323583+git-thinh@users.noreply.github.com>
2018-09-16 21:47:52 +0700

    Initial commit

---
+++ OffScreenCefGlue/Program.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using Xilium.CefGlue;
+
+namespace OffScreenCefGlue
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Load CEF. This checks for the correct CEF version.
+            CefRuntime.Load();
+
+            // Start the secondary CEF process.
+            var cefMainArgs = new CefMainArgs(new string[0]);
+            var cefApp = new DemoCefApp();
+
+            // This is where the code path divereges for child processes.
+            if (CefRuntime.ExecuteProcess(cefMainArgs, cefApp) != -1)
+            {
+                Console.Error.WriteLine("CefRuntime could not the secondary process.");
+            }
+
+            // Settings for all of CEF (e.g. process management and control).
+            var cefSettings = new CefSettings
+            {
+                SingleProcess = false,
+                MultiThreadedMessageLoop = true
+            };
+
+            // Start the browser process (a child process).
+            CefRuntime.Initialize(cefMainArgs, cefSettings, cefApp);
+
+            // Instruct CEF to not render to a window at all.
+            CefWindowInfo cefWindowInfo = CefWindowInfo.Create();
+            cefWindowInfo.SetAsOffScreen(IntPtr.Zero);
+
+            // Settings for the browser window itself (e.g. should JavaScript be enabled?).
+            var cefBrowserSettings = new CefBrowserSettings();
+
+            // Initialize some the cust interactions with the browser process.
+            // The browser window will be 1280 x 720 (pixels).
+            var cefClient = new DemoCefClient(1280, 720);
+
+            // Start up the browser instance.
+            string url = "http://www.reddit.com/";
+            CefBrowserHost.CreateBrowser(cefWindowInfo, cefClient, cefBrowserSettings, url);
+
+            // Hang, to let the browser to do its work.
+            Console.WriteLine("Press a key at any time to end the program.");
+            Console.ReadKey();
+
+            // Clean up CEF.
+            CefRuntime.Shutdown();
+            
+        }
+    }
+
+    internal class DemoCefApp : CefApp
+    {
+    }
+
+    internal class DemoCefClient : CefClient
+    {
+        private readonly DemoCefLoadHandler _loadHandler;
+        private readonly DemoCefRenderHandler _renderHandler;
+
+        public DemoCefClient(int windowWidth, int windowHeight)
+        {
+            _renderHandler = new DemoCefRenderHandler(windowWidth, windowHeight);
+            _loadHandler = new DemoCefLoadHandler();
+        }
+
+        protected override CefRenderHandler GetRenderHandler()
+        {
+            return _renderHandler;
+        }
+
+        protected override CefLoadHandler GetLoadHandler()
+        {
+            return _loadHandler;
+        }
+    }
+
+    internal class DemoCefLoadHandler : CefLoadHandler
+    {
+        protected override void OnLoadStart(CefBrowser browser, CefFrame frame)
+        {
+            // A single CefBrowser instance can handle multiple requests
+            //   for a single URL if there are frames (i.e. <FRAME>, <IFRAME>).
+            if (frame.IsMain)
+            {
+                Console.WriteLine("START: {0}", browser.GetMainFrame().Url);
+            }
+        }
+
+        protected override void OnLoadEnd(CefBrowser browser, CefFrame frame, int httpStatusCode)
+        {
+            if (frame.IsMain)
+            {
+                Console.WriteLine("END: {0}, {1}", browser.GetMainFrame().Url, httpStatusCode);
+            }
+        }
+    }
+
+    internal class DemoCefRenderHandler : CefRenderHandler
+    {
+        private readonly int _windowHeight;
+        private readonly int _windowWidth;
+
+        public DemoCefRenderHandler(int windowWidth, int windowHeight)
+        {
+            _windowWidth = windowWidth;
+            _windowHeight = windowHeight;
+        }
+
+        protected override bool GetRootScreenRect(CefBrowser browser, ref CefRectangle rect)
+        {
+            return GetViewRect(browser, ref rect);
+        }
+
+        protected override bool GetScreenPoint(CefBrowser browser, int viewX, int viewY, ref int screenX, ref int screenY)
+        {
+            screenX = viewX;
+            screenY = viewY;
+            return true;
+        }
+
+        protected override bool GetViewRect(CefBrowser browser, ref CefRectangle rect)
+        {
+            rect.X = 0;
+            rect.Y = 0;
+            rect.Width = _windowWidth;
+            rect.Height = _windowHeight;
+            return true;
+        }
+
+        protected override bool GetScreenInfo(CefBrowser browser, CefScreenInfo screenInfo)
+        {
+            return false;
+        }
+
+        protected override void OnPopupSize(CefBrowser browser, CefRectangle rect)
+        {
+        }
+
+        protected override void OnPaint(CefBrowser browser, CefPaintElementType type, CefRectangle[] dirtyRects, IntPtr buffer, int width, int height)
+        {
+            // Save the provided buffer (a bitmap image) as a PNG.
+            var bitmap = new Bitmap(width, height, width*4, PixelFormat.Format32bppRgb, buffer);
+            bitmap.Save("LastOnPaint.png", ImageFormat.Png);
+        }
+
+        protected override void OnCursorChange(CefBrowser browser, IntPtr cursorHandle)
+        {
+        }
+
+        protected override void OnScrollOffsetChanged(CefBrowser browser)
+        {
+        }
+    }
+}
